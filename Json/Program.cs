1c926a3077a1cb887b96684dcd6fec9098c4116a
Максим Шиян <maksim.shiyann@mail.ru>
2019-11-29 20:24:16 +0700

    Commit

--- Json/Program.cs
+++ Json/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Json
+using System.Net.Http;
+using Newtonsoft.Json;
+namespace JSON
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://iss.moex.com/iss/securities.json?start=100");
+            var task = client.GetStringAsync("https://iss.moex.com/iss/securities.json?start=100");
+            var json = task.Result;
+            var root = JsonConvert.DeserializeObject<Security>(json);
+            var securities = root.securities.data.Select(
+                d => new Security
+                {
+                    id = Convert.ToInt32(d[0]),
+                    secid = d[1] as string,
+                    shortname = d[2] as string,
+                    regnumber = d[3] as string,
+                    name = d[4] as string,
+                    isin = d[5] as string,
+                    is_traded = Convert.ToInt32(d[6]),
+                    emitent_id = Convert.ToInt32(d[7]),
+                    emitent_title = d[8] as string,
+                    emitent_inn = d[9] as string,
+                    emitent_okpo = d[10] as string,
+                    gosreg = d[11] as string,
+                    type = d[12] as string,
+                    group = d[13] as string,
+                    primary_boardid = d[14] as string,
+                    marketprice_boardid = d[15] as string,
+                }
+                ).ToArray();
+            foreach (var item in securities)
+            {
+                Console.WriteLine(item);
+            }
         }
+
     }
 }
---
+++ Json/RootObject.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    public class Rootobject
+    {
+        public Securities securities { get; set; }
+    }
+    public class Metadata
+    {
+        public Id id { get; set; }
+        public Secid secid { get; set; }
+        public Shortname shortname { get; set; }
+        public Regnumber regnumber { get; set; }
+        public Name name { get; set; }
+        public Isin isin { get; set; }
+        public Is_Traded is_traded { get; set; }
+        public Emitent_Id emitent_id { get; set; }
+        public Emitent_Title emitent_title { get; set; }
+        public Emitent_Inn emitent_inn { get; set; }
+        public Emitent_Okpo emitent_okpo { get; set; }
+        public Gosreg gosreg { get; set; }
+        public Type type { get; set; }
+        public Group group { get; set; }
+        public Primary_Boardid primary_boardid { get; set; }
+        public Marketprice_Boardid marketprice_boardid { get; set; }
+    }
+    public class Security
+    {
+        public Securities securities { get; set; }
+        public int id { get; set; }
+        public string secid { get; set; }
+        public string shortname { get; set; }
+        public string regnumber { get; set; }
+        public string name { get; set; }
+        public string isin { get; set; }
+        public int is_traded { get; set; }
+        public int emitent_id { get; set; }
+        public string emitent_title { get; set; }
+        public string emitent_inn { get; set; }
+        public string emitent_okpo { get; set; }
+        public string gosreg { get; set; }
+        public string type { get; set; }
+        public string group { get; set; }
+        public string primary_boardid { get; set; }
+        public string marketprice_boardid { get; set; }
+    }
+
+    public class Securities
+    {
+        public string[] columns { get; set; }
+        public object[][] data { get; set; }
+    }
+
+    public class Id
+    {
+        public string type { get; set; }
+    }
+
+    public class Secid
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Shortname
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Regnumber
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Name
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Isin
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Is_Traded
+    {
+        public string type { get; set; }
+    }
+
+    public class Emitent_Id
+    {
+        public string type { get; set; }
+    }
+
+    public class Emitent_Title
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Emitent_Inn
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Emitent_Okpo
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Gosreg
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Type
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Group
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Primary_Boardid
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+    public class Marketprice_Boardid
+    {
+        public string type { get; set; }
+        public int bytes { get; set; }
+        public int max_size { get; set; }
+    }
+
+}
