cbe12d3567e28c3bda970b220bc0362f02407283
Максим Шиян <maksim.shiyann@mail.ru>
2020-02-28 21:43:58 +0700

    commit

---
+++ Exchange/Model/History.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Model
+{
+    class History
+    {
+        public string secId { get; set; }
+        public double close { get; set; }
+    }
+}
