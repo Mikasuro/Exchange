e2f9d91333655b4730f1c2ecdc99c2bbac1c9802
Блок Иван Николаевич <BlokIN@sibupk.su>
2020-03-10 23:31:31 +0700

    Исправлено построение графика с прогнозом

--- Exchange/SecurityPaper.xaml.cs
+++ Exchange/SecurityPaper.xaml.cs
@@ -15,26 +15,30 @@ using System.Globalization;
 using Microsoft.ML;
 using Microsoft.ML.Transforms.TimeSeries;
 using System.Collections.Generic;
+using LiveCharts.Defaults;
+using LiveCharts.Configurations;
 
 namespace Exchange
 {
+    public class DateModel
+    {
+        public System.DateTime DateTime { get; set; }
+        public double Value { get; set; }
+    }
     public partial class Window1 : Window, INotifyPropertyChanged
     {
         bool _closed;
-        private string[] _labels;
         private readonly Security _security;
+        public Func<double, string> XFormatter { get; set; } =
+            value =>
+            {
+                if (value >= 0) { return new System.DateTime((long)(value * TimeSpan.FromDays(1).Ticks)).ToString("dd.MM"); }
+                return "";
+            };
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public SeriesCollection SeriesCollection { get; set; } = new SeriesCollection();
-
-        public string[] Labels { get => _labels;
-            set
-            {
-                _labels = value;
-                OnPropertyChanged();
-            }
-        }
+        public SeriesCollection SeriesCollection { get;set; }
 
         public Window1()
         {
@@ -46,9 +50,14 @@ namespace Exchange
         {
             _closed = true;
         }
+
         public Window1(Security security)
         {
             InitializeComponent();
+            var dayConfig = Mappers.Xy<DateModel>()
+                .X(dayModel => (double)dayModel.DateTime.Ticks / TimeSpan.FromDays(1).Ticks)
+                .Y(dayModel => dayModel.Value);
+            SeriesCollection = new SeriesCollection(dayConfig);
             DataContext = this;
             _security = security;
             Closed += Window1_Closed;
@@ -60,17 +69,21 @@ namespace Exchange
         {
             HistoryLoader history = new HistoryLoader();
             var hs = history.LoadHistory(_security.secId, DateTime.Today.AddDays(-60).ToString("yyyy-MM-dd").Replace('.', '-'), DateTime.Today.ToString("yyyy-MM-dd").Replace('.', '-'));
-            var prices = hs.Select(s => s.close);
+            var prices = hs.Select(s => new DateModel
+            {
+                DateTime = DateTime.ParseExact(s.tradeDate, "yyyy-MM-dd", CultureInfo.InvariantCulture),
+                Value = s.close
+            }).ToArray();
+
             var date = hs.Select(
                 d => DateTime.ParseExact(d.tradeDate, "yyyy-MM-dd", CultureInfo.InvariantCulture).ToString("dd MMM")
                 )
                 .ToArray();
             var length = date.Length;
-            for (int i = 0; i < date.Length; i++)
-            {
+            for (int i = 0; i < date.Length; i++) {
 
             }
-            Labels = date;
+            //Labels = date;
             Dispatcher.Invoke(() =>
             {
                 var series = new LineSeries
@@ -80,8 +93,10 @@ namespace Exchange
                     PointGeometrySize = 3,
                     PointForeground = Brushes.Gray
                 };
-                series.Values = new ChartValues<double>(prices);
+
+                series.Values = new ChartValues<DateModel>(prices);
                 SeriesCollection.Add(series);
+
             });
         }
 
@@ -99,8 +114,7 @@ namespace Exchange
                 tbLastChange.Text = mk[0].lastChange.ToString();
                 tbTime.Text = mk[0].time.ToString();
             });
-            if (_closed != true)
-            {
+            if (_closed != true) {
                 Thread.Sleep(5000);
                 Task.Run(() => LoadData());
             }
@@ -114,26 +128,41 @@ namespace Exchange
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             HistoryLoader history = new HistoryLoader();
-            var hs = history.LoadHistory(_security.secId, DateTime.Today.AddDays(-60).ToString("yyyy-MM-dd").Replace('.', '-'), DateTime.Today.ToString("yyyy-MM-dd").Replace('.', '-'));
+            DateTime currentDate = DateTime.Today;
+            var hs = history.LoadHistory(_security.secId, currentDate.AddDays(-60).ToString("yyyy-MM-dd").Replace('.', '-'), currentDate.ToString("yyyy-MM-dd").Replace('.', '-'));
             var prices = hs.Select(p => (float)p.close).ToArray();
             var ml = new MLContext();
             var dataView = ml.Data.LoadFromEnumerable(hs);
             var inputColumnName = "floatClose";
             var outputColumnName = nameof(ForecastResult.Forecast);
-            var model = ml.Forecasting.ForecastBySsa(outputColumnName,inputColumnName, 10, 20, prices.Length, 5);
+            var model = ml.Forecasting.ForecastBySsa(outputColumnName, inputColumnName, 10, 20, prices.Length, 5);
             var transformer = model.Fit(dataView);
-            var forecastEngine = transformer.CreateTimeSeriesEngine<History,ForecastResult>(ml);
+            var forecastEngine = transformer.CreateTimeSeriesEngine<History, ForecastResult>(ml);
             var forecast = forecastEngine.Predict();
+
+
+            //последняя точка основного графика:
+            var lastHistoryPoint = new DateModel { DateTime = currentDate = DateTime.Parse(hs.Last().tradeDate), Value = hs.Last().floatClose };
+
             Dispatcher.Invoke(() =>
-            {
+            {                
+                List<DateModel> predictedPrices = new List<DateModel>();
+                predictedPrices.Add(lastHistoryPoint);
+                for( int i = 0;i < forecast.Forecast.Length; ++i) {
+                    DateModel dm = new DateModel { DateTime = currentDate.AddDays(i+1), Value = forecast.Forecast[i] };
+                    predictedPrices.Add(dm);
+                }
+                
                 var forecastSeries = new LineSeries
                 {
-                    Title = "Forecast",
+
+                    Title = "Прогноз",
                     LineSmoothness = 0,
                     PointGeometrySize = 3,
-                    PointForeground = Brushes.Red
+                    PointForeground = Brushes.Red,
+
                 };
-                forecastSeries.Values = new ChartValues<float>(forecast.Forecast);
+                forecastSeries.Values = new ChartValues<DateModel>(predictedPrices);
                 SeriesCollection.Add(forecastSeries);
             });
         }
