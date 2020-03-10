3128dbd7f98e5d6f4ab50a5bfdb423abfbe7c2d3
Максим Шиян <maksim.shiyann@mail.ru>
2020-03-11 02:24:20 +0700

    Commit

--- Exchange/SecurityPaper.xaml.cs
+++ Exchange/SecurityPaper.xaml.cs
@@ -141,7 +141,7 @@ namespace Exchange
             var forecast = forecastEngine.Predict();
 
 
-            //последняя точка основного графика:
+            //последняя точка основного графика1:
             var lastHistoryPoint = new DateModel { DateTime = currentDate = DateTime.Parse(hs.Last().tradeDate), Value = hs.Last().floatClose };
 
             Dispatcher.Invoke(() =>
